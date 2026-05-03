1ca3e552b1ae2f2a4c7c908cbeae0c1d062a313b
Willem van Veelen <social@garagebook.nl>
2026-05-04 01:13:14 +0200

    Improve thematic internal content links

--- app/Support/InternalContentLinks.php
+++ app/Support/InternalContentLinks.php
@@ -10,6 +10,31 @@ class InternalContentLinks
 {
     public const FEATURED_PAGE_SLUG = 'universeel-onderhoudsboekje-kopen-dit-is-het-beste-alternatief-2026';
 
+    private const RELATED_BLOG_SLUGS = [
+        'digitaal-onderhoudsboekje-voor-je-motor-wat-is-het-en-hoe-werkt-het' => [
+            'waarom-een-universeel-onderhoudsboekje-achterhaald-is-en-wat-je-beter-kunt-gebruiken',
+            'digitaal-onderhoudsboekje-voor-je-motor-van-papieren-boekje-naar-complete-historie',
+            'hoe-een-complete-onderhoudshistorie-de-verkoopwaarde-van-je-motor-verhoogt',
+        ],
+        'waarom-een-universeel-onderhoudsboekje-achterhaald-is-en-wat-je-beter-kunt-gebruiken' => [
+            'digitaal-onderhoudsboekje-voor-je-motor-wat-is-het-en-hoe-werkt-het',
+            'digitaal-onderhoudsboekje-voor-je-motor-van-papieren-boekje-naar-complete-historie',
+            'hoe-een-complete-onderhoudshistorie-de-verkoopwaarde-van-je-motor-verhoogt',
+        ],
+        'hoe-een-complete-onderhoudshistorie-de-verkoopwaarde-van-je-motor-verhoogt' => [
+            'motor-verkopen-dit-doet-een-goede-onderhoudshistorie-met-je-prijs',
+            'onderhoudshistorie-van-je-motor-kwijt-dit-kun-je-doen-en-voorkomen-in-de-toekomst',
+            'digitaal-onderhoudsboekje-voor-je-motor-wat-is-het-en-hoe-werkt-het',
+        ],
+    ];
+
+    private const FEATURED_PAGE_RELATED_BLOG_SLUGS = [
+        'waarom-een-universeel-onderhoudsboekje-achterhaald-is-en-wat-je-beter-kunt-gebruiken',
+        'digitaal-onderhoudsboekje-voor-je-motor-wat-is-het-en-hoe-werkt-het',
+        'digitaal-onderhoudsboekje-voor-je-motor-van-papieren-boekje-naar-complete-historie',
+        'hoe-een-complete-onderhoudshistorie-de-verkoopwaarde-van-je-motor-verhoogt',
+    ];
+
     public static function featuredPage(): ?Page
     {
         return Page::query()
@@ -19,6 +44,12 @@ class InternalContentLinks
 
     public static function relatedBlogsForBlog(Blog $blog, int $limit = 3): Collection
     {
+        $relatedSlugs = static::RELATED_BLOG_SLUGS[$blog->slug] ?? null;
+
+        if ($relatedSlugs !== null) {
+            return static::blogsBySlugPriority($relatedSlugs, $limit);
+        }
+
         return Blog::query()
             ->whereNotNull('published_at')
             ->whereKeyNot($blog->getKey())
@@ -29,10 +60,33 @@ class InternalContentLinks
 
     public static function relatedBlogsForFeaturedPage(int $limit = 4): Collection
     {
+        $relatedBlogs = static::blogsBySlugPriority(static::FEATURED_PAGE_RELATED_BLOG_SLUGS, $limit);
+
+        if ($relatedBlogs->isNotEmpty()) {
+            return $relatedBlogs;
+        }
+
         return Blog::query()
             ->whereNotNull('published_at')
             ->latest('published_at')
             ->limit($limit)
             ->get();
+    }
+
+    private static function blogsBySlugPriority(array $slugs, int $limit): Collection
+    {
+        $slugOrder = array_values(array_unique($slugs));
+
+        $blogs = Blog::query()
+            ->whereNotNull('published_at')
+            ->whereIn('slug', $slugOrder)
+            ->get()
+            ->keyBy('slug');
+
+        return collect($slugOrder)
+            ->map(fn (string $slug) => $blogs->get($slug))
+            ->filter()
+            ->take($limit)
+            ->values();
     }
 }
--- tests/Feature/BlogInternalLinksTest.php
+++ tests/Feature/BlogInternalLinksTest.php
@@ -20,26 +20,44 @@ class BlogInternalLinksTest extends TestCase
         ]);
 
         $primaryBlog = Blog::query()->create([
-            'title' => 'Eerste blog',
-            'slug' => 'eerste-blog',
+            'title' => 'Digitaal onderhoudsboekje voor je motor',
+            'slug' => 'digitaal-onderhoudsboekje-voor-je-motor-wat-is-het-en-hoe-werkt-het',
             'excerpt' => 'Eerste',
+            'content' => '<p>Body</p>',
+            'published_at' => now()->subDays(2),
+        ]);
+
+        $relatedBlog = Blog::query()->create([
+            'title' => 'Universeel onderhoudsboekje',
+            'slug' => 'waarom-een-universeel-onderhoudsboekje-achterhaald-is-en-wat-je-beter-kunt-gebruiken',
+            'excerpt' => 'Tweede',
             'content' => '<p>Body</p>',
             'published_at' => now()->subDay(),
         ]);
 
-        $relatedBlog = Blog::query()->create([
-            'title' => 'Tweede blog',
-            'slug' => 'tweede-blog',
-            'excerpt' => 'Tweede',
+        $secondaryRelatedBlog = Blog::query()->create([
+            'title' => 'Onderhoudshistorie en verkoopwaarde',
+            'slug' => 'hoe-een-complete-onderhoudshistorie-de-verkoopwaarde-van-je-motor-verhoogt',
+            'excerpt' => 'Derde',
             'content' => '<p>Body</p>',
             'published_at' => now(),
+        ]);
+
+        Blog::query()->create([
+            'title' => 'Niet relevant maar nieuwer',
+            'slug' => 'niet-relevant-maar-nieuwer',
+            'excerpt' => 'Vierde',
+            'content' => '<p>Body</p>',
+            'published_at' => now()->addMinute(),
         ]);
 
         $response = $this->get('/blogs/' . $primaryBlog->slug);
 
         $response->assertOk();
         $response->assertSee('/blogs/' . $relatedBlog->slug, false);
+        $response->assertSee('/blogs/' . $secondaryRelatedBlog->slug, false);
         $response->assertSee('/' . $featuredPage->slug, false);
+        $response->assertDontSee('/blogs/niet-relevant-maar-nieuwer', false);
     }
 
     public function test_featured_page_shows_related_blog_links(): void
@@ -51,16 +69,25 @@ class BlogInternalLinksTest extends TestCase
         ]);
 
         $blog = Blog::query()->create([
-            'title' => 'Onderhoud slim bijhouden',
-            'slug' => 'onderhoud-slim-bijhouden',
+            'title' => 'Universeel onderhoudsboekje',
+            'slug' => 'waarom-een-universeel-onderhoudsboekje-achterhaald-is-en-wat-je-beter-kunt-gebruiken',
             'excerpt' => 'Excerpt',
             'content' => '<p>Body</p>',
             'published_at' => now(),
+        ]);
+
+        Blog::query()->create([
+            'title' => 'Niet geselecteerde blog',
+            'slug' => 'niet-geselecteerde-blog',
+            'excerpt' => 'Excerpt',
+            'content' => '<p>Body</p>',
+            'published_at' => now()->addMinute(),
         ]);
 
         $response = $this->get('/' . $page->slug);
 
         $response->assertOk();
         $response->assertSee('/blogs/' . $blog->slug, false);
+        $response->assertDontSee('/blogs/niet-geselecteerde-blog', false);
     }
 }
